53d846d73d17adc7bf27d1c5cec94b4fe297539b
Chen <taokarlchen@gmail.com>
2016-03-04 16:45:44 -0600

    new

---
+++ src/Simulator.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Chen
+ *
+ */
+public class Simulator {
+
+	/**
+	 * 
+	 */
+	public Simulator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
